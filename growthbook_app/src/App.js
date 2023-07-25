8bb8ed831d86d33354e93d8119e1c75e5d445672
Khalil <khalilfoulks@gmail.com>
2023-07-25 13:55:15 -0400

    add growthbook feature

--- growthbook_app/src/App.js
+++ growthbook_app/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { IfFeatureEnabled } from '@growthbook/growthbook-react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <IfFeatureEnabled feature="show-logo">
+          <img src={logo} className="App-logo" alt="logo" />
+        </IfFeatureEnabled>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
